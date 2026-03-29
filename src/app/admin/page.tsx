2edf87f2568c2c84c225cfda4b56e00a4fc53cfa
Tanmay <tanmay@Tanmays-MacBook-Pro.local>
2026-03-29 11:55:12 +0530

    cleanup: removed papaparse completely

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,7 +2,7 @@
 import { useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
 import { supabase } from "@/lib/supabase";
-// import Papa from "papaparse"
+import Papa from "papaparse"
 
 export default function AdminPage() {
   const router = useRouter();
@@ -35,25 +35,25 @@ export default function AdminPage() {
     if (data) setTasks(data);
   };
 
-  // const handleCSVUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   const file = event.target.files?.[0];
-  //   if (!file) return;
+ const handleCSVUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
+  const file = event.target.files?.[0];
+   if (!file) return;
 
-  //   Papa.parse(file, {
-  //     header: true,
-  //     skipEmptyLines: true,
-  //     complete: async (results) => {
-  //       const { error } = await supabase.from("team").insert(results.data as any);
+    Papa.parse(file, {
+      header: true,
+      skipEmptyLines: true,
+     complete: async (results) => {
+        const { error } = await supabase.from("team").insert(results.data as any);
 
-  //       if (error) {
-  //         alert("Upload failed: " + error.message);
-  //       } else {
-  //         alert("Team uploaded successfully!");
-  //         fetchMembers();
-  //       }
-  //     },
-  //   });
-  // };
+        if (error) {
+          alert("Upload failed: " + error.message);
+        } else {
+          alert("Team uploaded successfully!");
+           fetchMembers();
+        }
+       },
+       });
+  };
 
   return (
     <main className="min-h-screen bg-[#000000] px-6 py-14">
